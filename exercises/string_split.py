2006e93649add5796765e5493739208867d2ed3a
xunCai84 <849076823@qq.com>
2025-12-16 15:11:06 +0800

    这是string_split

--- exercises/string_split.py
+++ exercises/string_split.py
@@ -19,7 +19,7 @@ def extract_keywords(text):
     """
     # 请在下方编写代码
     # 使用split()方法分割字符串，返回关键词列表
-    pass
+    return text.split()
 
 def parse_csv_line(csv_line):
     """
@@ -33,7 +33,7 @@ def parse_csv_line(csv_line):
     """
     # 请在下方编写代码
     # 使用split()方法分割CSV行，返回字段列表
-    pass
+    return csv_line.split(',')
 
 def extract_name_and_domain(email):
     """
@@ -47,4 +47,5 @@ def extract_name_and_domain(email):
     """
     # 请在下方编写代码
     # 使用split()方法分割电子邮件地址，返回用户名和域名的元组
-    pass 
+    parts = email.split('@')
+    return (parts[0], parts[1])
