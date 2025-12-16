5043665e54fc807db93212bff943572764cdc478
xunCai84 <849076823@qq.com>
2025-12-16 14:59:56 +0800

    完成了regex练习

--- exercises/regex_match.py
+++ exercises/regex_match.py
@@ -17,7 +17,8 @@ def find_emails(text):
     """
     # 实现你的代码: 使用正则表达式查找所有邮箱地址
     # 邮箱格式通常为: username@domain.com
-    pass
+    pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    return re.findall(pattern, text)
 
 
 def is_valid_phone_number(phone):
@@ -36,7 +37,10 @@ def is_valid_phone_number(phone):
         bool: 如果是有效的手机号码则返回True，否则返回False
     """
     # 实现你的代码: 验证手机号码是否合法
-    pass
+    pattern = r'^1[3-9]\d{9}$'
+    if re.match(pattern, phone):
+        return True
+    return False
 
 
 def extract_urls(text):
@@ -51,4 +55,5 @@ def extract_urls(text):
     """
     # 实现你的代码: 使用正则表达式提取所有URL
     # 需要考虑http://和https://开头的URL
-    pass 
+    pattern = r'https?://[^\s]+'
+    return re.findall(pattern, text)
