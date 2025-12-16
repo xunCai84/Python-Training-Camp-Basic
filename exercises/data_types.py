927c9a162356a1989ecd42200af41aa6e4604991
xunCai84 <849076823@qq.com>
2025-12-16 14:02:16 +0800

    这是function_params

--- exercises/data_types.py
+++ exercises/data_types.py
@@ -26,4 +26,4 @@ def get_data_types():
     # 4. 创建一个布尔值变量，值为 True
     bool_var = True
     # 5. 将这些变量作为元组返回
-    return (integer_var, float_var, string_var, bool_var)
+    return (integer_var, float_var, string_var, bool_var)   
